9d5ceeb77553f68a4201db416e923e1531863ccb
NIDA <NIDA@DESKTOP-86PDROH>
2024-10-25 02:41:17 -0700

    Add project files.

---
+++ PasswordGeneratorApp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PasswordGeneratorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGeneratePassword = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(378, 113);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(186, 20);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Password";
+            // 
+            // btnGeneratePassword
+            // 
+            this.btnGeneratePassword.Location = new System.Drawing.Point(316, 171);
+            this.btnGeneratePassword.Name = "btnGeneratePassword";
+            this.btnGeneratePassword.Size = new System.Drawing.Size(113, 23);
+            this.btnGeneratePassword.TabIndex = 2;
+            this.btnGeneratePassword.Text = "GeneratePassword";
+            this.btnGeneratePassword.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGeneratePassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGeneratePassword;
+    }
+}
+
---
+++ PasswordGeneratorApp/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PasswordGeneratorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Optional: If you want to execute something when the form loads, you can add this method.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGeneratePassword_Click(object sender, EventArgs e)
+        {
+            
+            txtPassword.Text = GeneratePassword();
+        }
+
+     
+        private string GeneratePassword()
+        {
+            // Fixed components of the password
+            string digits = "002";  // 2 digits from your registration number
+            string lettersFromName = "im";  // Second letters from "Nida" and "Eman"
+            string movieChars = "TO";  // Example characters from "THE FORT MUSTANG"
+            string specialChars = "@$!&";  // Special characters excluding '#'
+
+            // Pool of characters excluding '#'
+            string characterPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" + specialChars;
+
+            // Random number generator
+            Random random = new Random();
+            StringBuilder passwordBuilder = new StringBuilder(digits + lettersFromName + movieChars);
+
+            // Add remaining random characters to make the password 14 characters long
+            while (passwordBuilder.Length < 14)
+            {
+                char nextChar = characterPool[random.Next(characterPool.Length)];
+                if (nextChar != '#') // Exclude '#'
+                {
+                    passwordBuilder.Append(nextChar);
+                }
+            }
+
+            // Shuffle the password for better randomness
+            string password = Shuffle(passwordBuilder.ToString());
+
+            // Validate password with regex to ensure it meets requirements (14 characters and no '#')
+            if (Regex.IsMatch(password, @"^(?!.*#).{14}$"))
+            {
+                return password;
+            }
+            else
+            {
+                return "Password generation failed.";
+            }
+        }
+
+        // Method to shuffle the characters in the string
+        private string Shuffle(string input)
+        {
+            char[] array = input.ToCharArray();
+            Random random = new Random();
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                // Swap characters
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            return new string(array);
+        }
+    }
+}
